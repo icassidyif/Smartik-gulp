e0232a29fa46aee28e5d56b180489710f49078a3
Ihor <icassidyif@gmail.com>
2020-06-01 21:43:48 +0300

    garden block + popup details

--- app/js/particles/popup.js
+++ app/js/particles/popup.js
@@ -15,4 +15,20 @@ $(document).ready(function () {
     },
     removalDelay: 300
   });
+
+  $('.popup-with-move-anim').magnificPopup({
+    type: 'inline',
+
+    fixedContentPos: false,
+    fixedBgPos: true,
+
+    overflowY: 'auto',
+
+    closeBtnInside: true,
+    preloader: false,
+
+    midClick: true,
+    removalDelay: 300,
+    mainClass: 'my-mfp-slide-bottom'
+  });
 })
