692e36714303e22fc542038401e2605b5e738274
Ihor <icassidyif@gmail.com>
2020-06-07 21:35:45 +0300

    fill all forms axept camp detail

--- Smartik-gulp/js/app.js
+++ Smartik-gulp/js/app.js
@@ -145,6 +145,114 @@ $('#gardenForm').validate({
   // }
 
 });
+$('#campForm').validate({
+  rules: {
+    parentName: {
+      required: true,
+      minlength: 2
+    },
+    parentSurname: {
+      required: true,
+      minlength: 2
+    },
+    phone: {
+      required: true,
+      customphone: true
+    },
+    childName: {
+      required: true,
+      minlength: 2
+    },
+    childSurname: {
+      required: true,
+      minlength: 2
+    },
+    age: {
+      required: true,
+      number: true
+    }
+  },
+  messages: {
+    parentName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    parentSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    phone: {
+      required: "Це обов'язкове поле",
+      customphone: 'Невірний номер телефону'
+    },
+    childName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    childSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    age: {
+      required: "Це обов'язкове поле",
+      number: 'Введіть число'
+    }
+  }
+});
+$('#languagesForm').validate({
+  rules: {
+    parentName: {
+      required: true,
+      minlength: 2
+    },
+    parentSurname: {
+      required: true,
+      minlength: 2
+    },
+    phone: {
+      required: true,
+      customphone: true
+    },
+    childName: {
+      required: true,
+      minlength: 2
+    },
+    childSurname: {
+      required: true,
+      minlength: 2
+    },
+    age: {
+      required: true,
+      number: true
+    }
+  },
+  messages: {
+    parentName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    parentSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    phone: {
+      required: "Це обов'язкове поле",
+      customphone: 'Невірний номер телефону'
+    },
+    childName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    childSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    age: {
+      required: "Це обов'язкове поле",
+      number: 'Введіть число'
+    }
+  }
+});
 $(document).ready(function () {
   $('.popup-gallery').magnificPopup({
     delegate: 'a',
--- app/js/particles/validate.js
+++ app/js/particles/validate.js
@@ -76,4 +76,116 @@ $('#gardenForm').validate({
   // submitHandler: function () {
   //   alert('OK')
   // }
+});
+
+
+$('#campForm').validate({
+  rules: {
+    parentName: {
+      required: true,
+      minlength: 2
+    },
+    parentSurname: {
+      required: true,
+      minlength: 2
+    },
+    phone: {
+      required: true,
+      customphone: true
+    },
+    childName: {
+      required: true,
+      minlength: 2
+    },
+    childSurname: {
+      required: true,
+      minlength: 2
+    },
+    age: {
+      required: true,
+      number: true
+    }
+  },
+  messages: {
+    parentName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    parentSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    phone: {
+      required: "Це обов'язкове поле",
+      customphone: 'Невірний номер телефону'
+    },
+    childName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    childSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    age: {
+      required: "Це обов'язкове поле",
+      number: 'Введіть число'
+    }
+  }
+});
+
+
+$('#languagesForm').validate({
+  rules: {
+    parentName: {
+      required: true,
+      minlength: 2
+    },
+    parentSurname: {
+      required: true,
+      minlength: 2
+    },
+    phone: {
+      required: true,
+      customphone: true
+    },
+    childName: {
+      required: true,
+      minlength: 2
+    },
+    childSurname: {
+      required: true,
+      minlength: 2
+    },
+    age: {
+      required: true,
+      number: true
+    }
+  },
+  messages: {
+    parentName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    parentSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    phone: {
+      required: "Це обов'язкове поле",
+      customphone: 'Невірний номер телефону'
+    },
+    childName: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке ім'я"
+    },
+    childSurname: {
+      required: "Це обов'язкове поле",
+      minlength: "Надто коротке прізвище"
+    },
+    age: {
+      required: "Це обов'язкове поле",
+      number: 'Введіть число'
+    }
+  }
 })
